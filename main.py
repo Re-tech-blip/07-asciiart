01ab2761193d41e55843ca8b7e40228b0f1ce50a
Re-tech-blip <remi.hanoteau-cherfils@edu.esiee.fr>
2025-11-20 10:02:21 +0000

    Remi Hanoteau ascii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #### Imports et définition des variables globales
-
+""" Module principal pour ASCII Art.
+Contient des fonctions pour encoder et afficher des chaînes de caractères."""
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -9,7 +10,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères
+    passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +19,28 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
 
-    return [ ]
+    result = []
+    current_char = s[0]
+    count = 1
+
+    for c in s[1:]:
+        if c == current_char:
+            count += 1
+        else:
+            result.append((current_char, count))
+            current_char = c
+            count = 1
+    result.append((current_char, count))  # ajout du dernier groupe
+
+    return result
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument
+    selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +48,23 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
+    # compter le nombre de caractères identiques au début
+    count = 1
+    while count < len(s) and s[count] == s[0]:
+        count += 1
+    # encodage du premier bloc
+    first_tuple = [(s[0], count)]
+    # appel récursif sur le reste
+    return first_tuple + artcode_r(s[count:])
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
 
 #### Fonction principale
 
 
 def main():
+    """Effectue des tests du modules"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
